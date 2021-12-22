498a808b30d980ededeb2ca13380ee176939a359
Thomas Bacheley <79910466+ThomasBacheley@users.noreply.github.com>
2021-12-22 16:21:07 +0100

    add credits page

---
+++ credits.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/now-ui-kit.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,600,700,800,900&display=swap" rel="stylesheet">
+    <title>Crédits</title>
+    <meta content="Yweelon.fr" property="og:title" />
+    <meta content="Site d'Yweelon" property="og:description" />
+    <meta content="http://yweelon.fr" property="og:url" />
+    <meta content="https://cdn.discordapp.com/attachments/770357581549535233/922704792260866058/BotLogo.png" property="og:image" />
+    <meta content="#ffa500" data-react-helmet="true" name="theme-color" />
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-transparent">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <img src="assets/menuIcon.svg" width="20px" height="20px" style="max-width: none !important;">
+        </button>
+        <a href="index.php">
+            <img src="assets/BotLogo.png" width="40" height="40">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 20px !important">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Accueil</a>
+                </li>
+                <div class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Bot
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="Hellbot.php">Hellbot</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="GIT_bot.php">GIT Bot <span class="sr-only">(actuel)</span></a>
+                    </div>
+                </div>
+                <div class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Guardian Tale
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="GT_herosheet.php">Hero Sheet</a>
+                        <a class="dropdown-item" href="GT_addhero.php">Add Hero</a>
+                        <a class="dropdown-item" href="GT_updatehero.php">Update Hero</a>
+                    </div>
+                </div>
+            </ul>
+            <div class="nav-item">
+                <a class="nav-link" href="http://yweelon.fr/phpmyadmin">PHPMyAdmin</a>
+            </div>
+            <div class="nav-item">
+                <a class="nav-link" href="https://github.com/ThomasBacheley">Github</a>
+            </div>
+            <button id="connexion_button" class="btn login-btn btn-outline-accent my-2 my-sm-0" style="font-size: 10px !important;font-family: poppins !important;">Connexion</button>
+        </div>
+    </nav>
+    <!---->
+    <div class="heading">
+    <p>Site design par <a style="color: #ffa500;" href="https://github.com/Nouridio">nouridio</a></p>
+    <br/>
+    <p><a style="color: #ffa500;" href="https://icons8.com/icon/86840/mail">Mail icon</a> par Icons8</p>
+    <br/>
+    <p>Données GuardianTale basé sur des connaisance personnel du jeu ,<br/>ainsi que prise sur <a style="color: #ffa500;" href="https://guardiantalesguides.com/">guardiantalesguides.com</a> et <a style="color: #ffa500;" href="https://heavenhold.com/">heavenhold.com</a></p>
+    </div>
+    <br /><br /><br /><br /><br />
+    <div class="page_end">
+        <br />
+        <div class="footer">
+            <div class="bot-footer">
+                <a href="credits.php">
+                    <img src="assets/BotLogoWord.png" width="150" style="margin-left:-45px;"><br />
+                </a>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/now-ui-kit.min.js"></script>
+    <script>
+        var username = '<?php echo $_SESSION['username']; ?>';
+
+        var btn_connexion = document.getElementById('connexion_button')
+
+        if (username !== "") {
+            btn_connexion.innerText = username
+            btn_connexion.setAttribute('title', 'Dashboard');
+            btn_connexion.setAttribute('data-bs-toggle', 'tooltip');
+            btn_connexion.setAttribute('data-bs-placement', 'bottom');
+            btn_connexion.setAttribute('onclick', 'window.location.href=\'/dashboard.php\'')
+        } else {
+            btn_connexion.setAttribute('onclick', 'window.location.href=\'/login.php\'')
+        }
+    </script>
+</body>
+
+</html>
